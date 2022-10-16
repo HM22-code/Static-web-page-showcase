a47c584b0984f7b0088d7738db6cdeae8d329b2d
HM22-code <H.M-22@outlook.fr>
2022-10-16 18:36:48 +0200

    HelloWorld first use JSX syntax React component

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,11 @@
-const title = React.createElement('h1', {}, 'Hello There !');
-ReactDOM.render(title,document.getElementById('HelloWorld_container'));
+function render(){
+    const title = 
+    <div>
+    <h1>Hello There !</h1>
+    <p>My first react component with JSX syntax</p>
+    </div>
+
+    ReactDOM.render(title,document.getElementById('HelloWorld_container'));
+}
+
+render();
