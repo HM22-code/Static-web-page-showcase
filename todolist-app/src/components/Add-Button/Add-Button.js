cedd83d73ec91e0b4a67635f544bfb45ab9ef82c
HM22-code <H.M-22@outlook.fr>
2022-10-23 13:35:18 +0200

    First create-react-app

---
+++ todolist-app/src/components/Add-Button/Add-Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Add-Button.module.css';
+
+const AddButton = () => (
+  <div className={styles.AddButton}>
+    AddButton Component
+  </div>
+);
+
+AddButton.propTypes = {};
+
+AddButton.defaultProps = {};
+
+export default AddButton;
