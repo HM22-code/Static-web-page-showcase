ae3a7e9689825c4e9965bfd37e2e18bcb1ca7e13
HM22-code <H.M-22@outlook.fr>
2022-11-09 23:37:59 +0100

    Adding Titles

--- todolist-app/src/components/TopBar/TopBar.js
+++ todolist-app/src/components/TopBar/TopBar.js
@@ -4,7 +4,11 @@ import styles from './TopBar.module.css'
 const TopBar = () => {
   return (
     <div className={styles.TopBar}>
-        TopBar
+        <h1><a href='#BoxContainer'>title</a></h1>
+        <h1><a href='#BoxContainer'>title</a></h1>
+        <h1><a href='#BoxContainer'>title</a></h1>
+        <h1><a href='#BoxContainer'>title</a></h1>
+        <h1><a href='#BoxContainer'>title</a></h1>
     </div>
   )
 }
