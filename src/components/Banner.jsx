644d0ab133cdb1269905dd7179c597bc67dde6e8
HM22-code <H.M-22@outlook.fr>
2023-06-08 16:12:13 +0200

    Update banner style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Banner() {
 	return (
-		<div className="bg-wood-bark bg-cover p-20">
-			<h1 className="font-serif text-4xl text-white">Accro'Fun</h1>
+		<div className="bg-wood-bark bg-cover p-8 sm:p-16">
+			<h1 className="hidden font-serif text-4xl text-white sm:block">
+				Accro'Fun
+			</h1>
 			<h4 className="text-lg text-white">Ça vous branche?</h4>
 		</div>
 	);
