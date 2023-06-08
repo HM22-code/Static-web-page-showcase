e355a7749652b70666968c75aa29605e30d414ad
HM22-code <H.M-22@outlook.fr>
2023-06-08 09:49:04 +0200

    Update NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,34 +4,36 @@ function NavBar() {
 	return (
 		<header className="bg-white dark:bg-slate-800">
 			<nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-				<h1 className="text-slate-900 dark:text-white">Accro'Fun</h1>
+				<h1 className="text-slate-900 dark:text-white font-serif text-3xl">
+					Accro'Fun
+				</h1>
 				<div className="mx-auto flex flex-row justify-around gap-x-10">
 					<a
-						className="text-slate-900 dark:text-white underline decoration-emerald-600"
+						className="text-slate-900 dark:text-white border-b-2 border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
 						href="#Accueil"
 					>
 						Accueil
 					</a>
 					<a
-						className="text-slate-900 dark:text-white underline decoration-emerald-600"
+						className="text-slate-900 dark:text-white border-b-2 border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
 						href="#Parcours"
 					>
 						Parcours
 					</a>
 					<a
-						className="text-slate-900 dark:text-white underline decoration-emerald-600"
+						className="text-slate-900 dark:text-white border-b-2 border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
 						href="#Tarifs"
 					>
 						Tarifs & Réservation
 					</a>
 					<a
-						className="text-slate-900 dark:text-white underline decoration-emerald-600"
+						className="text-slate-900 dark:text-white border-b-2 border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
 						href="#Horaires"
 					>
 						Horaires
 					</a>
 					<a
-						className="text-slate-900 dark:text-white underline decoration-emerald-600"
+						className="text-slate-900 dark:text-white border-b-2 border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
 						href="#Contact"
 					>
 						Contact
