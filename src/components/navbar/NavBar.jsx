c61de38d0db15badb79d9cf3cd5b79ef3d79b0da
HM22-code <H.M-22@outlook.fr>
2023-06-08 10:37:12 +0200

    Installing Prettier and Tailwind plugin

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 function NavBar() {
 	return (
-		<header className="bg-white dark:bg-slate-800 border-b-2 border-slate-200">
-			<nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-				<h1 className="text-slate-900 dark:text-white font-serif text-3xl">
+		<header className="border-b-2 border-slate-200 bg-white dark:bg-slate-800">
+			<nav className="mx-auto flex max-w-7xl flex-col items-center justify-between p-6 md:flex-row lg:px-8">
+				<h1 className="font-serif text-3xl text-slate-900 dark:text-white">
 					Accro'Fun
 				</h1>
-				<div className="mx-auto flex flex-row justify-around gap-x-10">
+				<div className="mx-auto flex flex-col justify-around gap-x-10 md:flex-row ">
 					<a
-						className="text-slate-900 dark:text-white border-b border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
+						className="border-b border-solid border-slate-900 text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white"
 						href="#Accueil"
 					>
 						Accueil
 					</a>
 					<a
-						className="text-slate-900 dark:text-white border-b border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
+						className="border-b border-solid border-slate-900 text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white"
 						href="#Parcours"
 					>
 						Parcours
 					</a>
 					<a
-						className="text-slate-900 dark:text-white border-b border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
+						className="border-b border-solid border-slate-900 text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white"
 						href="#Tarifs"
 					>
 						Tarifs & Réservation
 					</a>
 					<a
-						className="text-slate-900 dark:text-white border-b border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
+						className="border-b border-solid border-slate-900 text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white"
 						href="#Horaires"
 					>
 						Horaires
 					</a>
 					<a
-						className="text-slate-900 dark:text-white border-b border-slate-900 border-solid hover:border-emerald-600 hover:text-emerald-600"
+						className="border-b border-solid border-slate-900 text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white"
 						href="#Contact"
 					>
 						Contact
