00159b437e3cea478c2e5d761f1e19bed55bea88
HM22-code <H.M-22@outlook.fr>
2023-06-10 17:29:41 +0200

    Fix dark mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 function NavBar() {
 	return (
-		<header className="border-b-2 border-slate-200 bg-white dark:bg-slate-800">
+		<header className="border-b-2 border-slate-200 bg-white dark:border-slate-600 dark:bg-slate-800">
 			<nav className="mx-auto flex max-w-7xl flex-col items-center justify-between p-6 md:flex-row lg:px-8">
 				<h1 className="pb-0 font-serif text-3xl text-slate-900 dark:text-white sm:pb-4 md:pb-0">
 					Accro'Fun
 				</h1>
 				<div className="mx-auto hidden flex-col justify-center gap-x-5 text-base sm:visible sm:flex sm:flex-row sm:gap-y-2 sm:text-sm md:text-base  lg:gap-x-10">
 					<a
-						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900"
+						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900 dark:sm:border-white"
 						href="#Accueil"
 					>
 						Accueil
 					</a>
 					<a
-						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900"
+						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900 dark:sm:border-white"
 						href="#Parcours"
 					>
 						Parcours
 					</a>
 					<a
-						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900"
+						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900 dark:sm:border-white"
 						href="#Tarifs"
 					>
 						Tarifs & Réservation
 					</a>
 					<a
-						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900"
+						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900 dark:sm:border-white"
 						href="#Horaires"
 					>
 						Horaires
 					</a>
 					<a
-						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900"
+						className="text-slate-900 hover:border-emerald-600 hover:text-emerald-600 dark:border-white dark:text-white sm:border-b sm:border-solid sm:border-slate-900 dark:sm:border-white"
 						href="#Contact"
 					>
 						Contact
