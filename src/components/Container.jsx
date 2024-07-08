12cbf92c3e70d2c761ae3b82be065305a5e3d5be
HM22-code <H.M-22@outlook.fr>
2024-07-08 18:02:23 +0200

    Adding Container component

---
+++ src/components/Container.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import parkour from "../assets/img/parkour.jpg";
+import zipline from "../assets/img/zipline.jpg";
+import home from "../assets/img/home.jpg";
+
+function Container() {
+	return (
+		<div 
+			className="flex flex-col justify-center p-5 align-middle"
+			data-aos="fade-up"
+			data-aos-easing="linear"
+			data-aos-duration="1500"
+		>
+			<Carousel className="h-full w-full" dynamicHeight>
+				<div>
+					<img
+						src={parkour}
+						alt="Parkour"
+						className="h-full w-full object-cover"
+					/>
+				</div>
+				<div>
+					<img
+						src={zipline}
+						alt="Zipline"
+						className="h-full w-full object-cover"
+					/>
+				</div>
+				<div>
+					<img
+						src={home}
+						alt="Home"
+						className="h-full w-full object-cover"
+					/>
+				</div>
+			</Carousel>
+		</div>
+	);
+}
+
+export default Container;
