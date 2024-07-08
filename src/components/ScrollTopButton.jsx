baf3cb6486e69386b94efd97e1ca7ec203976ac7
HM22-code <H.M-22@outlook.fr>
2024-07-08 18:04:13 +0200

    Hide button on mobile

--- src/components/ScrollTopButton.jsx
+++ src/components/ScrollTopButton.jsx
@@ -12,7 +12,7 @@ function ScrollTopButton() {
 
 	return (
 		<button
-			className="fixed bottom-5 right-5 flex h-12 w-12 cursor-pointer justify-center rounded-full bg-white align-middle hover:bg-slate-300 dark:bg-white"
+			className="hidden lg:flex fixed bottom-5 right-5 h-12 w-12 cursor-pointer justify-center rounded-full bg-white align-middle hover:bg-slate-300 dark:bg-white"
 			onClick={scrollUp}
 		>
 			<img src={arrowup} alt="Arrow-up icon" />
